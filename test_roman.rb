1ad273fe37fbd95d10237db680591a881f5c1052
aljarrell <alan.k.jarrell@gmail.com>
2018-10-16 11:21:07 -0400

    tested for a couple numbers

--- test_roman.rb
+++ test_roman.rb
@@ -11,6 +11,17 @@ class My_test < Minitest::Test
     assert_equal(Hash, roman_numeral.class)
   end
 
+  def test_for_i
+    assert_equal("I", roman_numeral.fetch(1))
+  end
+
+  def test_for_iv
+    assert_equal("IV",roman_numeral.fetch(4))
+  end
+  
+  def test_for_v
+    assert_equal("V", roman_numeral.fetch(5))
+  end
 
 
 
